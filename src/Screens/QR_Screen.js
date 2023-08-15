72df254cc6fe8e25a62f167256ec7bccd6f69235
Tarun Negi <73269234+tarunnegi196@users.noreply.github.com>
2023-08-15 13:04:45 +0530

    added camera

--- src/Screens/QR_Screen.js
+++ src/Screens/QR_Screen.js
@@ -1,14 +1,72 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
-const QR_Screen = () => {
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanning, setScanning] = useState(false);
+  const [scannedData, setScannedData] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanning(false);
+    setScannedData(data);
+  };
+
+  const toggleScanner = () => {
+    if (!scanning) {
+      setScannedData(null);
+    }
+    setScanning(!scanning);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting permission to access camera...</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera.</Text>;
+  }
+
   return (
-    <View>
-      <Text>QR_Screen</Text>
+    <View style={styles.container}>
+      {scanning ? (
+        <BarCodeScanner
+          onBarCodeScanned={scanning ? handleBarCodeScanned : undefined}
+          style={StyleSheet.absoluteFillObject}
+        />
+      ) : (
+        <View style={styles.messageContainer}>
+          <Text style={styles.messageText}>
+            {scannedData
+              ? `Scanned Data: ${scannedData}`
+              : 'Tap the button to start scanning'}
+          </Text>
+          <Button title={scanning ? 'Stop Scanning' : 'Start Scanning'} onPress={toggleScanner} />
+        </View>
+      )}
     </View>
   );
-};
+}
 
-export default QR_Screen;
-
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  messageContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  messageText: {
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+});
