432ce6589cbf8f2785003c7b3f703a3680ff2cca
Affaan Kidwai <kidwaiaffaan@gmail.com>
2023-08-15 11:05:53 +0530

    added files

---
+++ App.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import ParentNavigator from "./src/Navigation/ParentNavigation";
+
+const queryClient = new QueryClient();
+
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <ParentNavigator />
+      </NavigationContainer>
+    </QueryClientProvider>
+  );
+}
