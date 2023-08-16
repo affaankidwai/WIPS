9dcdf1bbe10574397a774cebecbd57df0de60214
Affaan Kidwai <kidwaiaffaan@gmail.com>
2023-08-16 20:39:41 +0530

    splash screen added

---
+++ src/Screens/MicScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const MicScreen = () => {
+  return (
+    <View>
+      <Text>MicScreen</Text>
+    </View>
+  );
+};
+
+export default MicScreen;
+
+const styles = StyleSheet.create({});
